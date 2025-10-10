4f29f73062529f1b2e399d62f91897f930b9348b
Vladislav Kurganov <vkurganov05@gmail.com>
2025-10-10 22:17:38 +0300

    added PokemonEnum with stats and types

--- scripts/core/enums/Pokemon.cs
+++ scripts/core/enums/Pokemon.cs
@@ -52,3 +52,62 @@ public enum PokemonStat
     Accuracy,
     Evasion
 }
+
+
+public static class PokemonEnum
+{
+    public static readonly Dictionary<string, PokemonType> TypeMap = new()
+    {
+        { "normal", PokemonType.Normal},
+        { "fire", PokemonType.Fire},
+        { "water", PokemonType.Water},
+        { "grass", PokemonType.Grass},
+        { "electric", PokemonType.Electric},
+        { "ice", PokemonType.Ice},
+        { "fighting", PokemonType.Fighting},
+        { "poison", PokemonType.Poison},
+        { "ground", PokemonType.Ground},
+        { "flying", PokemonType.Flying},
+        { "psychic", PokemonType.Psychic},
+        { "bug", PokemonType.Bug},
+        { "rock", PokemonType.Rock},
+        { "ghost", PokemonType.Ghost},
+        { "dragon", PokemonType.Dragon},
+        { "dark", PokemonType.Dark},
+        { "steel", PokemonType.Steel},
+        { "fairy", PokemonType.Fairy},
+    };
+
+
+    public static readonly Dictionary<string, PokemonAilment> AilmentMap = new()
+    {
+        { "none", PokemonAilment.None },
+        { "burn", PokemonAilment.Burn },
+        { "freeze", PokemonAilment.Freeze },
+        { "paralysis", PokemonAilment.Paralysis },
+        { "poison", PokemonAilment.Poison },
+        { "toxic", PokemonAilment.Toxic },
+        { "sleep", PokemonAilment.Sleep },
+        { "confusion", PokemonAilment.Confusion },
+        { "trap", PokemonAilment.Trap },
+        { "leech-seed", PokemonAilment.LeechSeed },
+        { "disable", PokemonAilment.Disable },
+        { "unknown", PokemonAilment.Unknown },
+
+    };
+
+    public static readonly Dictionary<string, PokemonStat> StatMap = new()
+    {
+        { "hp", PokemonStat.Hp },
+        { "attack", PokemonStat.Attack },
+        { "defence", PokemonStat.Defence },
+        { "special-attack", PokemonStat.SpecialAttack },
+        { "special-defense", PokemonStat.SpecialDefence },
+        { "speed", PokemonStat.Speed },
+        { "accuracy", PokemonStat.Accuracy },
+        { "evasion", PokemonStat.Evasion },
+
+
+    };
+
+}
